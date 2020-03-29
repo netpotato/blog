e963c38e068e65fcd5c4ce581215daa3d70cbae2
hezhenyu <hezhenyu@dianjiutong.com>
2020-03-29 13:46:34 +0800

    隐藏模板ICP

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -108,7 +108,8 @@
 </article>
 
 <footer>
-    <p>Design by <a href="javascript:;">Potato个人博客</a> <a href="javascript:;">蜀ICP备11002373号-1</a></p>
+    <!-- <p>Design by <a href="javascript:;">Potato个人博客</a> <a href="javascript:;">蜀ICP备11002373号-1</a></p> -->
+    <p>Design by <a href="javascript:;">Potato个人博客</a> <a href="javascript:;">ICP</a></p>
 </footer>
 
 <a href="javascript:;" class="cd-top">Top</a>
