a22043add1dfbac519ed000143342d76c66a19db
hezhenyu <hezhenyu@dianjiutong.com>
2020-04-13 09:40:38 +0800

    添加备案号

--- resources/views/home/layouts/footer.blade.php
+++ resources/views/home/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 
 <footer>
     <!-- <p>Design by <a href="javascript:;">Potato个人博客</a> <a href="javascript:;">蜀ICP备11002373号-1</a></p> -->
-    <p>Design by <a href="javascript:;">Potato个人博客</a> <a href="javascript:;">ICP</a></p>
+    <p>Design by <a href="javascript:;">Potato个人博客</a>备案号： <a href="javascript:;">陇ICP备20001089号</a></p>
 </footer>
 
 <a href="javascript:;" class="cd-top">Top</a>
